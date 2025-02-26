d251d069a2bc6e5d8964d4d2a4a42f394e885aea
Anton Ramus <anton.ramus@hotmail.com>
2025-02-26 10:30:10 +0100

    added education and getter

--- src/application/models/Exam.java
+++ src/application/models/Exam.java
@@ -5,11 +5,22 @@ public class Exam {
     private final String examName;
     private final String term;
     private final String examType;
+    private final Education education;
 
-    public Exam(int examId, String examName, String term, String examType) {
+    public Exam(int examId, String examName, String term, String examType, Education education) {
         this.examId = examId;
         this.examName = examName;
         this.term = term;
         this.examType = examType;
+        this.education = education;
+
+    }
+
+    public int getExamId() {
+        return examId;
+    }
+
+    public Education getEducation() {
+        return education;
     }
 }
