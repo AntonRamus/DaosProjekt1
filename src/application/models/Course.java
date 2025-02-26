02068cfd8f6ee385819b11dac71e8d46efca2061
Anton Ramus <anton.ramus@hotmail.com>
2025-02-26 09:23:31 +0100

    class attributes and constructors

--- src/application/models/Course.java
+++ src/application/models/Course.java
@@ -1,4 +1,14 @@
 package src.application.models;
 
 public class Course {
+    private final int courseId;
+    private final String courseName;
+    private final int numberOfLessons;
+
+    public Course(int courseId, String courseName, int numberOfLessons) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.numberOfLessons = numberOfLessons;
+    }
+
 }
--- src/application/models/Student.java
+++ src/application/models/Student.java
@@ -1,4 +1,13 @@
 package src.application.models;
 
 public class Student {
+    private final int studentId;
+    private String studentName;
+    private boolean active;
+
+    public Student(int studentId, String studentName, boolean active) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.active = active;
+    }
 }
