e9f87fd4227dcc55b52ed1e069029d46f3d055c0
Anton Ramus <anton.ramus@hotmail.com>
2025-02-27 09:57:23 +0100

    hotboxing

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -2,6 +2,7 @@ package src.gui;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -19,82 +20,62 @@ public class MainWindow extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Karakterregistreringssystem");
-        GridPane gridPane = new GridPane();
-        initContent(gridPane);
-        Scene scene = new Scene(gridPane);
+        VBox vBox = new VBox(10);
+        initContent(vBox);
+        Scene scene = new Scene(vBox);
         stage.setScene(scene);
         stage.show();
     }
 
-    private void initContent(GridPane gridPane) {
-        gridPane.setPadding(new Insets(20));
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
+    private void initContent(VBox vBox) {
+        vBox.setPadding(new Insets(20));
 
-
-        HBox topHbox = new HBox();
-        topHbox.setSpacing(10);
-        gridPane.add(topHbox,0,0);
+        HBox topHbox = new HBox(10);
+        HBox middleHBox = new HBox(10);
+        HBox buttomHBox = new HBox(10);
 
         ComboBox<Education> uddannelseComboBox = new ComboBox<>();
-        uddannelseComboBox.setOnAction(e->updateUddannelseComboBox(uddannelseComboBox));
-        topHbox.getChildren().add(uddannelseComboBox);
-
         ComboBox<Exam> eksamenComboBox = new ComboBox<>();
-        eksamenComboBox.setOnAction(e->updateEksamenComboBox(eksamenComboBox, uddannelseComboBox));
-        topHbox.getChildren().add(eksamenComboBox);
-
-        VBox leftVBox = new VBox();
-        leftVBox.setSpacing(10);
-        gridPane.add(leftVBox,0,1);
-
         ListView<Student> studentListView = new ListView<>();
-        studentListView.getItems().setAll();
-        leftVBox.getChildren().add(studentListView);
-
-        VBox rightVBox = new VBox();
-        rightVBox.setSpacing(10);
-        gridPane.add(rightVBox,1,0);
-
-        Button button1 = new Button("Button1");
-        button1.setOnAction(null);
-        rightVBox.getChildren().add(button1);
-
-        Button button2 = new Button("Button2");
-        button2.setOnAction(null);
-        rightVBox.getChildren().add(button2);
-
-        HBox hBox = new HBox();
-        hBox.setSpacing(10);
-        gridPane.add(hBox,0,2);
-
         Button button3 = new Button("Afvikel Eksamen");
-        button3.setOnAction(event-> afvikelEksamen(uddannelseComboBox.getSelectionModel().getSelectedItem()));
-        hBox.getChildren().add(button3);
-
         Button button4 = new Button("Opret Eksamens Forsøg");
-        button4.setOnAction(e->opretEksamensForsøgAction(null));
-        hBox.getChildren().add(button4);
 
+
+        updateUddannelseComboBox(uddannelseComboBox);
+        uddannelseComboBox.setOnAction(e -> updateEksamenComboBox(eksamenComboBox, uddannelseComboBox));
+
+        studentListView.getItems().setAll();
+
+        button3.setOnAction(event -> afvikelEksamen(uddannelseComboBox.getSelectionModel().getSelectedItem()));
+        button4.setOnAction(e -> opretEksamensForsøgAction(null));
+
+        topHbox.getChildren().addAll(uddannelseComboBox, eksamenComboBox);
+        middleHBox.getChildren().add(studentListView);
+        buttomHBox.getChildren().addAll(button3, button4);
+
+        topHbox.setAlignment(Pos.CENTER);
+        middleHBox.setAlignment(Pos.CENTER);
+        buttomHBox.setAlignment(Pos.CENTER);
+        vBox.getChildren().addAll(topHbox, middleHBox, buttomHBox);
     }
 
-    private void afvikelEksamen(Education education){
-        if (education!=null){
-        new AfvikelEksamenWindow(education).showAndWait();
+    private void afvikelEksamen(Education education) {
+        if (education != null) {
+            new AfvikelEksamenWindow(education).showAndWait();
         }
     }
 
-    private void opretEksamensForsøgAction(Student student){
-        if (student!=null) {
+    private void opretEksamensForsøgAction(Student student) {
+        if (student != null) {
             new OpretEksamensForsøgWindow(student).showAndWait();
         }
     }
 
-    private void updateUddannelseComboBox(ComboBox<Education> comboBox){
+    private void updateUddannelseComboBox(ComboBox<Education> comboBox) {
         comboBox.getItems().setAll(Storage.getEducations());
     }
 
-    private void updateEksamenComboBox(ComboBox<Exam> comboBox1, ComboBox<Education> comboBox2){
+    private void updateEksamenComboBox(ComboBox<Exam> comboBox1, ComboBox<Education> comboBox2) {
         comboBox1.getItems().setAll(Storage.getExamsOnEducation(comboBox2.getSelectionModel().getSelectedItem()));
     }
 }
